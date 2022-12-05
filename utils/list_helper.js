929f35a4ffa609b615c0eff744fd493c1d8c46e0
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2022-12-05 20:01:30 -0300

    :sparkles: feat: 4.4: helper functions and unit tests, step2
    Create a totalLikes function and some tests for it

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,5 +1,14 @@
 const dummy = (blogs) => 1
 
+const totalLikes = (blogs) => {
+  if (blogs === undefined) {
+    return 0
+  }
+
+  return blogs.reduce((sum, blog) => blog.likes + sum, 0)
+}
+
 module.exports = {
   dummy,
+  totalLikes,
 }
